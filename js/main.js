fd451e32ee2983837b988e8c4afe79db168b75a0
fraggota <ramus99.99@mail.ru>
2021-01-19 20:05:40 +0700

    add slider and refactoring

--- js/main.js
+++ js/main.js
@@ -1,57 +1,33 @@
-var cart = {}; 
+function slider() {
+    $('.next').click(function() {
+		var currentImage = $('.image.curry');
+		var currentImageIndex = $('.image.curry').index();
+		var nextImageIndex = currentImageIndex + 1;
+		var nextImage = $('.image').eq(nextImageIndex);
+		currentImage.fadeOut(1000);
+		currentImage.removeClass('curry');
 
-function init() {
-    $.getJSON("goods.json", goodsOut); 
-}
-   
-function goodsOut(data) {
-    var out='';
-    for (var key in data) {
-        out +='<div class="cart">';
-        out +=`<img class="imgs" src="images/${data[key].img}" alt="">`;
-        out +=`<p class="name">${data[key].name}</p>`;
-        out +=`<div class="descr">${data[key].descr}</div>`;
-        out +=`<div class="cost">${data[key].cost} РУБ</div>`;
-        out +=`<button class="add-to-cart" data-id="${key}">Купить</button>`;
-        out +='</div>';
-    }
-    $('.goods-out').html(out);
-    $('.add-to-cart').on('click', addToCart)
-}
+		if(nextImageIndex == ($('.image:last').index()+1)) {
+			$('.image').eq(0).fadeIn(1000);
+			$('.image').eq(0).addClass('curry');
+		} else {
+			nextImage.fadeIn(1000);
+			nextImage.addClass('curry');
+		}
+	});
 
-function addToCart() {
-    var id = $(this).attr('data-id');
-    if (cart[id]==undefined) {
-        cart[id] = 1;
-    }
-    else {
-        cart[id]++;
-    }
-    showCart();
-    saveCart();
-}
-
-function saveCart() {
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
-
-function showCart() {
-    var out="";
-    for (var key in cart) {
-        out += key + ' --- ' + cart[key] + '; ';
-    }
-    $('.mcart').html(out);
-}
-
-function loadCart() {
-    if (localStorage.getItem('cart')) {
-        cart = JSON.parse(localStorage.getItem('cart'));
-        showCart();
-    }    
+	$('.previous').click(function() {
+		var currentImage = $('.image.curry');
+		var currentImageIndex = $('.image.curry').index();
+		var prevImageIndex = currentImageIndex - 1;
+		var prevImage = $('.image').eq(prevImageIndex);
+		currentImage.fadeOut(1000);
+		currentImage.removeClass('curry');
+		prevImage.fadeIn(1000);
+		prevImage.addClass('curry');
+    });
 }
 
 $(document).ready(function () {
-    init();
-    loadCart();
+    slider();
 });
-
---
+++ js/menu.js
@@ -0,0 +1,57 @@
+var cart = {}; 
+
+function init() {
+    $.getJSON("goods.json", goodsOut); 
+}
+   
+function goodsOut(data) {
+    var out='';
+    for (var key in data) {
+        out +='<div class="cart">';
+        out +=`<img class="imgs" src="images/${data[key].img}" alt="">`;
+        out +=`<p class="name">${data[key].name}</p>`;
+        out +=`<div class="descr">${data[key].descr}</div>`;
+        out +=`<div class="cost">${data[key].cost} РУБ</div>`;
+        out +=`<button class="add-to-cart" data-id="${key}">Купить</button>`;
+        out +='</div>';
+    }
+    $('.goods-out').html(out);
+    $('.add-to-cart').on('click', addToCart)
+}
+
+function addToCart() {
+    var id = $(this).attr('data-id');
+    if (cart[id]==undefined) {
+        cart[id] = 1;
+    }
+    else {
+        cart[id]++;
+    }
+    showCart();
+    saveCart();
+}
+
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function showCart() {
+    var out="";
+    for (var key in cart) {
+        out += key + ' --- ' + cart[key] + '; ';
+    }
+    $('.mini-cart').html(out);
+}
+
+function loadCart() {
+    if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+        showCart();
+    }    
+}
+
+$(document).ready(function () {
+    init();
+    loadCart();
+});
+
