7da1e2b268636ecd15e03cc6914348a50a364aa3
fraggota <ramus99.99@mail.ru>
2021-05-29 20:23:49 +0700

    delete comments

--- head.php
+++ head.php
@@ -1,16 +1,12 @@
  
-<meta charset="UTF-8"> <!-- Кодировка документа -->
+<meta charset="UTF-8">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Meta-тег viewport сообщает браузеру о том, как именно обрабатывать размеры страницы, и изменять её масштаб.  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- Устанавливаем связь со шрифтом -->
 <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:400,700&amp;subset=cyrillic-ext" rel="stylesheet">
 
-<!-- Заголовок документа, который отображается в заголовке окна браузера или на вкладке страницы -->
 <title>Delivery</title>
 
-<!-- Иконка сайта в адресной строке браузера -->
 <link rel="icon" type="image/png" href="images/logo1.png">
 
-<!-- Путь к оснонвую файлу стилей -->
 <link rel="stylesheet" href="css/style.css">
